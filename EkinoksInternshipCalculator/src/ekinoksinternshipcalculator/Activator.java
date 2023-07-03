2b8c7cc616558041fe8ef67226a2d8bb4d1698ce
Çağan Tuncer <cagan.tuncer@ug.bilkent.edu.tr>
2023-07-03 15:43:57 +0300

    deneme

--- EkinoksInternshipCalculator/src/ekinoksinternshipcalculator/Activator.java
+++ EkinoksInternshipCalculator/src/ekinoksinternshipcalculator/Activator.java
@@ -19,6 +19,7 @@ public class Activator implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 		System.out.println(this.getClass().getName() + " stopped.");
+		//
 	}
 
 }
